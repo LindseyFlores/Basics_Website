2bff4ab667fc826843da1a158c577154ca280b68
Lindsey Flores <profe.linaf.1@gmail.com>
2024-04-16 11:38:43 -0400

    node_modules placed inside .gitignore, app.js created for express server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //Creating a basic Express server
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));  // Serve static files from public directory
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
