71fd79665d87aa744157d0b2a4e0fcb5904194b5
Lindsey Flores <profe.linaf.1@gmail.com>
2024-04-26 10:53:15 -0400

    minor app change hopefully this fixes

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 "use strict";
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
 const app = express();
-const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Middleware for parsing our JSON bodies
-app.use(express.json());  // This should be high up, before route handlers
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+//app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// View engine
-app.set("view engine", "ejs");
 
 // Importing routes
 const uploadRoute = require('./routes/upload.route');
@@ -19,21 +16,20 @@ const adminEditRoute = require('./routes/adminEdit.route');
 const productRoute = require('./routes/product.route');
 const cartRoute = require('./routes/cart.route');
 const userRouter = require("./routes/user.route");
-
-// routes
 app.use('/upload', uploadRoute);
 app.use('/edit', adminEditRoute);
 app.use('/products', productRoute);
 app.use('/cart', cartRoute);
 app.use(userRouter);
 
-// Root routes
+// render
 app.get("/", (req, res) => {
     res.render("login");
 });
 app.get("/register", (req, res) => {
     res.render("register");
 });
+//render for products?
 
 // Start server
 const PORT = 3000;
